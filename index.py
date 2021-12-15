668ba9cb5169c1c824b9c21b0dce220898be9aaf
XTmv <89165163+XTmv@users.noreply.github.com>
2021-12-15 15:32:03 +0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,153 @@
+
+print("""
+░▄▀▀▀▀▄░░▄▄
+█░░░░░░▀▀░░█░░░░░░▄░▄
+█░║░░░░██░████████████ 
+█░░░░░░▄▄░░█░░░░░░▀░▀
+░▀▄▄▄▄▀░░▀▀
+
+insta = @6g7r_here
+---------------
+index :  1,2,3,4 
+""")
+s=input('1,2,3,4: ')
+if('1') in s:
+  
+  name = input('your name : ')
+  bio = input('phrase: ')
+  AC = input('Account: ')
+  yehia = f'''                                                                                             
+<html dir="rtl">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+</head>
+
+<body text="#FF0000" bgcolor="#000000" background="https://64.media.tumblr.com/2f52fd0beb68c2dab19fec108539ce61/tumblr_p05hsu7kVg1vr26xwo1_400.gifv">
+
+<p align="center">‏ </p>
+<p align="center">&nbsp;</p>
+<p align="center"><span lang="en-us"><font size="7"><font color="#FF00FF">Hack</font><font color="#C0C0C0">ed</font>
+<font color="#CC0066">By</font> <font color="#9900CC">{name}</font></font></span></p>
+
+<p align="center"><img border="" src="https://64.media.tumblr.com/ff3a6ccc536e86a985390786ddae8338/tumblr_ndczd0zZRP1s4i43zo1_400.gifv?resize=500%2C324&ssl=1"</body></p>
+<p align="center"><font size="7" color="#FF00FF">{bio}</font></p>
+
+
+<p align="center"><span lang="en-us"><font size="7" color="#9900CC">{AC}</font></span></p>
+
+
+
+</html>
+'''
+  with open(f"index.txt", "a") as mix:
+    mix.write(f"{yehia}\n")
+    mix.close()
+ 
+
+
+ 
+if('2') in s:
+  name = input('your name: ')
+  bio = input('phrase: ')
+  AC = input('Account: ')
+  yehia = f'''                                                                                             
+<html dir="rtl">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+</head>
+
+
+<body bgcolor="#000000">
+
+
+<p align="center">‏ </p>
+<p align="center"><font size="6" color="#FF0000">Hacked By {name}</font></p>
+<p align="center">&nbsp;</p>
+<p align="center"><img border="" src="https://64.media.tumblr.com/ce2b17fd9be4c5923abc8c0f270d5197/tumblr_oilmfbyJGi1vf8f7eo1_500.gifv?resize=500%2C324&ssl=1"</body></p>
+<p align="center"><font color="#FF0000"><font size="6">{bio}</font>
+<p align="center"><font size="6" color="#FF0000">{AC}</font></p>
+
+
+
+
+
+</html>
+'''
+  with open(f"index2.txt", "a") as mix:
+    mix.write(f"{yehia}\n")
+    mix.close()
+ 
+
+
+  
+if('3') in s:
+  name = input('your name : ')
+  bio = input('phrase: ')
+  AC = input('Account: ')
+  yehia = f'''                                                                                             
+<html dir="rtl">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+</head>
+
+
+<body bgcolor="#000000" background="https://64.media.tumblr.com/4bf691792b1aa998efbfdd9ce549e3f5/tumblr_mngdugBvdG1qdje50o3_250.gifv">
+
+
+<p align="center">‏ </p>
+<p align="center"><font size="7" color="#00FFFF">Hacked By </font>
+<font size="7" color="#C0C0C0">{name}</font></p>
+<p align="center">&nbsp;</p>
+<p align="center"><img border="" src="https://64.media.tumblr.com/769866d448d782fb7f6e7c757af365dd/a12c06df917ca91f-96/s400x600/d780d010ad261f81acbacef25e92cf83c9c8e37d.gifv?resize=500%2C324&ssl=1"</body></p>
+<p align="center"><font color="#FFFF00"><font size="6">{bio}</font>
+<p align="center"><font size="6" color="#808080">{AC}</font></p>
+
+
+
+
+
+</html>
+'''
+  with open(f"index3.txt", "a") as mix:
+    mix.write(f"{yehia}\n")
+    mix.close()
+  
+
+
+  
+if('4') in s:
+  name = input('your name : ')
+  bio = input('phrase: ')
+  AC = input('Account: ')
+  yehia = f'''                                                                                             
+<html dir="rtl">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+</head>
+
+
+<body bgcolor="#000000" background="https://64.media.tumblr.com/f4ea3778980a663ed00ee1e4e422539c/tumblr_mubwwzKNj41sczu2fo1_400.gifv">
+
+
+<p align="center">‏ </p>
+<p align="center"><b><font size="7" color="#808080">Hacked By </font>
+<font size="7" color="#C0C0C0">{name}</font></b></p>
+<p align="center">&nbsp;</p>
+<p align="center"><img border="" src="https://64.media.tumblr.com/12b507902c3eaf6acdc63923bc82224b/b8ab02598cec8cac-5b/s500x750/1eb2664083ca01c6c49d04035dce02639d722f14.gifv?resize=500%2C324&ssl=1"</body></p>
+<p align="center"><font color="#FFFFFF"><font size="6">{bio}</font>
+<p align="center"><b><font size="6" color="#808080">{AC}</font></b></p>
+
+
+
+
+
+</html>
+'''
+  with open(f"index4.txt", "a") as mix:
+    mix.write(f"{yehia}\n")
+    mix.close()
+  
